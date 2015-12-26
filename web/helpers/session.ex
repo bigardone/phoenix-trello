da91c5c66179d307f09693c3fdbbf282231cbc20
Ricardo <bigardone@gmail.com>
2015-12-26 17:51:21 +0100

    Session helper

---
+++ web/helpers/session.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixTrello.Session do
+  alias PhoenixTrello.Repo
+  alias PhoenixTrello.User
+
+  def authenticate(%{"email" => email, "password" => password}) do
+    user = Repo.get_by(User, email: String.downcase(email))
+
+    case check_password(user, password) do
+      true -> {:ok, user}
+      _ -> :error
+    end
+  end
+
+  defp check_password(user, password) do
+    case user do
+      nil -> false
+      _ -> Comeonin.Bcrypt.checkpw(password, user.crypted_password)
+    end
+  end
+end
