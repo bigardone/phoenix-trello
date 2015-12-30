4d468c6739e553bb6c7288788d78da5fa72d88e8
Ricardo <bigardone@gmail.com>
2015-12-30 11:55:24 +0100

    Fixed join

--- web/models/board.ex
+++ web/models/board.ex
@@ -34,7 +34,7 @@ defmodule PhoenixTrello.Board do
 
   def for_user(user_id) do
     from board in Board,
-    join: user_boards in assoc(board, :user_boards),
+    left_join: user_boards in assoc(board, :user_boards),
     where: board.user_id == ^user_id or user_boards.user_id == ^user_id
   end
 end
