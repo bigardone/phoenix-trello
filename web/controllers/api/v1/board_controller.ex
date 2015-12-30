208d7a1a2f6d8b569ae5bc0a64e915c92b0826d7
Ricardo <bigardone@gmail.com>
2015-12-30 11:51:51 +0100

    Preload more assocs

--- web/controllers/api/v1/board_controller.ex
+++ web/controllers/api/v1/board_controller.ex
@@ -12,11 +12,11 @@ defmodule PhoenixTrello.BoardController do
 
     owned_boards = assoc(current_user, :owned_boards)
       |> Repo.all
-      |> Repo.preload([lists: :cards])
+      |> Repo.preload([:user, :invited_users, lists: :cards])
 
     invited_boards = assoc(current_user, :invited_boards)
       |> Repo.all
-      |> Repo.preload([lists: :cards])
+      |> Repo.preload([:user, :invited_users, lists: :cards])
 
     render(conn, "index.json", owned_boards: owned_boards, invited_boards: invited_boards)
   end
