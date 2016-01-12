2e4d6453b798d9fbb329a3b22706f6a28126074a
Ricardo <bigardone@gmail.com>
2016-01-12 09:50:32 +0100

    Card controller

---
+++ web/controllers/api/v1/card_controller.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixTrello.CardController do
+  use PhoenixTrello.Web, :controller
+
+  plug Guardian.Plug.EnsureAuthenticated, handler: PhoenixTrello.SessionController
+
+  alias PhoenixTrello.Repo
+  alias PhoenixTrello.Board
+
+  def show(conn, %{"board_id" => board_id, "id" => id}) do
+    current_user = Guardian.Plug.current_resource(conn)
+
+    card = Board.for_user(current_user.id)
+      |> Repo.get(board_id)
+      |> assoc(:cards)
+      |> Repo.get!(id)
+      |> Repo.preload([comments: [:user]])
+
+    render(conn, "show.json", card: card)
+  end
+end
