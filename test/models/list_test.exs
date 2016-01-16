d70522ccead58ce307ab8e59ead5e1b452e71773
Ricardo <bigardone@gmail.com>
2016-01-16 11:59:15 +0100

    List tests

--- test/models/list_test.exs
+++ test/models/list_test.exs
@@ -1,18 +1,47 @@
 defmodule PhoenixTrello.ListTest do
   use PhoenixTrello.ModelCase
 
-  alias PhoenixTrello.List
+  import PhoenixTrello.Factory
+
+  alias PhoenixTrello.{Repo, List}
 
   @valid_attrs %{name: "some content"}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = List.changeset(%List{}, @valid_attrs)
-    assert changeset.valid?
+  setup do
+    {:ok, %{board: create(:board)}}
   end
 
-  test "changeset with invalid attributes" do
-    changeset = List.changeset(%List{}, @invalid_attrs)
+  test "changeset with valid attributes", %{board: board} do
+    changeset = List.changeset(%List{board_id: board.id}, @valid_attrs)
+
+    assert changeset.valid?
+
+    list = Repo.insert!(changeset)
+    assert list.position == 1024
+  end
+
+  test "changeset with invalid attributes", %{board: board} do
+    changeset = List.changeset(%List{board_id: board.id}, @invalid_attrs)
+
     refute changeset.valid?
+  end
+
+  test "existing lists for the same board", %{board: board} do
+    count = 3
+
+    for i <- 1..(count-1) do
+      board
+      |> build_assoc(:lists)
+      |> List.changeset(%{name: "List #{i}"})
+      |> Repo.insert
+    end
+
+    {:ok, last_list} = board
+      |> build_assoc(:lists)
+      |> List.changeset(%{name: "Last"})
+      |> Repo.insert
+
+    assert last_list.position == 1024 * count
   end
 end
