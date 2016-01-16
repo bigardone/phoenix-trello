18aa775cda6c99865bb6afa5f26c0bf2ac5f2ef4
Ricardo <bigardone@gmail.com>
2016-01-16 11:59:27 +0100

    Added new factories

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule PhoenixTrello.Factory do
   use ExMachina.Ecto, repo: PhoenixTrello.Repo
 
-  alias PhoenixTrello.{User}
+  alias PhoenixTrello.{User, Board, List}
 
   def factory(:user) do
     %User{
@@ -9,6 +9,27 @@ defmodule PhoenixTrello.Factory do
       last_name: sequence(:last_name, &"Last #{&1}"),
       email: sequence(:email, &"email-#{&1}@foo.com"),
       encrypted_password: "12345678"
+    }
+  end
+
+  def factory(:board) do
+    %Board{
+      name: sequence(:name, &"Name #{&1}"),
+      user: build(:user)
+    }
+  end
+
+  def factory(:board_with_lists) do
+    %Board{
+      name: sequence(:name, &"Name #{&1}"),
+      user: build(:user),
+      lists: build_list(3, :list)
+    }
+  end
+
+  def factory(:list) do
+    list = %List{
+      name: sequence(:name, &"Name #{&1}")
     }
   end
 end
