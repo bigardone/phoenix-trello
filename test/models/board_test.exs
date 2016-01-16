504be2a0942ff8aa89787453c90b9f9c835a4039
Ricardo <bigardone@gmail.com>
2016-01-16 11:58:14 +0100

    Slug test

--- test/models/board_test.exs
+++ test/models/board_test.exs
@@ -1,14 +1,23 @@
 defmodule PhoenixTrello.BoardTest do
   use PhoenixTrello.ModelCase
 
+  import PhoenixTrello.Factory
+
   alias PhoenixTrello.Board
 
   @valid_attrs %{name: "some content"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
-    changeset = Board.changeset(%Board{}, @valid_attrs)
+    user = create(:user)
+    attributes = @valid_attrs
+      |> Map.put(:user_id, user.id)
+
+    changeset = Board.changeset(build(:board), attributes)
     assert changeset.valid?
+
+    %{slug: slug} = changeset.changes
+    assert slug == "some-content"
   end
 
   test "changeset with invalid attributes" do
