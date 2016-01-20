41fb0af7c771d246b8d1ceac65d9b5c205acfda6
Ricardo <bigardone@gmail.com>
2016-01-20 17:58:11 +0100

    Fixed migration error

--- priv/repo/migrations/20160115081022_update_boards_slugs.exs
+++ priv/repo/migrations/20160115081022_update_boards_slugs.exs
@@ -4,14 +4,18 @@ defmodule PhoenixTrello.Repo.Migrations.UpdateBoardsSlugs do
   alias PhoenixTrello.{Repo, Board}
 
   def change do
-    Repo.all(Board)
-    |> Enum.each(fn board ->
-      slug = board.name
-        |> String.downcase()
-        |> String.replace(~r/[^\w-]+/, "-")
-
-      Board.changeset(board, %{slug: slug})
-      |> Repo.update()
-    end)
+    # try do
+    #   Repo.all(Board)
+    #   |> Enum.each(fn board ->
+    #     slug = board.name
+    #       |> String.downcase()
+    #       |> String.replace(~r/[^\w-]+/, "-")
+    #
+    #     Board.changeset(board, %{slug: slug})
+    #     |> Repo.update()
+    #   end)
+    # catch
+    #   _,_ -> IO.puts("Error updating boards slugs")
+    # end
   end
 end
