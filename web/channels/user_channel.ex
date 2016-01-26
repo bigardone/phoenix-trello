882ffc12aab27a62d8fee799432e15d8b604be09
Ricardo <bigardone@gmail.com>
2016-01-26 16:33:23 +0100

    Socket and channel refactoring

--- web/channels/user_channel.ex
+++ web/channels/user_channel.ex
@@ -4,9 +4,10 @@ defmodule PhoenixTrello.UserChannel do
   def join("users:" <> user_id, _params, socket) do
     current_user = socket.assigns.current_user
 
-    cond do
-      String.to_integer(user_id) == current_user.id -> {:ok, socket}
-      true -> {:error, %{}}
+    if String.to_integer(user_id) == current_user.id do
+      {:ok, socket}
+    else
+      {:error, %{reason: "Invalid user"}}
     end
   end
 end
--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,7 +1,7 @@
 defmodule PhoenixTrello.UserSocket do
   use Phoenix.Socket
 
-  alias PhoenixTrello.{Repo, User}
+  alias PhoenixTrello.{GuardianSerializer}
 
   # Channels
   channel "boards:*", PhoenixTrello.BoardChannel
@@ -14,10 +14,12 @@ defmodule PhoenixTrello.UserSocket do
   def connect(%{"token" => token}, socket) do
     case Guardian.decode_and_verify(token) do
       {:ok, claims} ->
-        user_id = claims["sub"] |> String.replace("User:", "") |> String.to_integer
-        user = Repo.get!(User, user_id)
-
-        {:ok, assign(socket, :current_user, user)}
+        case GuardianSerializer.from_token(claims["sub"]) do
+          {:ok, user} ->
+            {:ok, assign(socket, :current_user, user)}
+          {:error, _reason} ->
+            :error
+        end
       {:error, _reason} ->
         :error
     end
