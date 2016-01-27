4e037ded4c6f4a8cce5b06fb0104c3f34fa36f2e
Ricardo <bigardone@gmail.com>
2016-01-27 17:35:30 +0100

    Added default value to query function

--- web/controllers/api/v1/card_controller.ex
+++ web/controllers/api/v1/card_controller.ex
@@ -7,11 +7,11 @@ defmodule PhoenixTrello.CardController do
 
   def show(conn, %{"board_id" => board_id, "id" => card_id}) do
     current_user = Guardian.Plug.current_resource(conn)
-    
+
     user_id = current_user.id
-    card = Card 
-           |> Card.get_by_user_and_board(card_id, user_id, board_id)
-           |> Repo.one!
+    card = Card
+     |> Card.get_by_user_and_board(card_id, user_id, board_id)
+     |> Repo.one!
 
     render(conn, "show.json", card: card)
   end
