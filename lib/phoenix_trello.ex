5cd0e664fd7b860f1c382fac448accc925f452a4
Ricardo <bigardone@gmail.com>
2016-02-05 15:25:46 +0100

    Added supervisor to children

--- lib/phoenix_trello.ex
+++ lib/phoenix_trello.ex
@@ -13,8 +13,7 @@ defmodule PhoenixTrello do
       worker(PhoenixTrello.Repo, []),
       # Here you could define other workers and supervisors as children
       # worker(PhoenixTrello.Worker, [arg1, arg2, arg3]),
-      worker(PhoenixTrello.BoardChannel.Monitor, [%{}]),
-
+      supervisor(PhoenixTrello.BoardChannel.Supervisor, []),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
