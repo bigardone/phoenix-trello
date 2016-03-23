867cf7c583598d23c66bb6b6e09693298ef39eec
Ricardo García Vega <bigardone@gmail.com>
2016-03-23 09:15:53 +0100

    Removed unnecessary function

--- web/controllers/api/v1/current_user_controller.ex
+++ web/controllers/api/v1/current_user_controller.ex
@@ -4,24 +4,10 @@ defmodule PhoenixTrello.CurrentUserController do
   plug Guardian.Plug.EnsureAuthenticated, handler: PhoenixTrello.SessionController
 
   def show(conn, _) do
-    case decode_and_verify_token(conn) do
-      { :ok, _claims } ->
-        user = Guardian.Plug.current_resource(conn)
+    user = Guardian.Plug.current_resource(conn)
 
-        conn
-        |> put_status(:ok)
-        |> render("show.json", user: user)
-
-      { :error, _reason } ->
-        conn
-        |> put_status(:not_found)
-        |> render(PhoenixTrello.SessionView, "error.json", error: "Not Found")
-    end
-  end
-
-  defp decode_and_verify_token(conn)  do
     conn
-    |> Guardian.Plug.current_token
-    |> Guardian.decode_and_verify
+    |> put_status(:ok)
+    |> render("show.json", user: user)
   end
 end
