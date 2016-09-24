c0b15bbe42170b66eef6afe3664840f2ae8b2534
Ricardo García Vega <bigardone@gmail.com>
2016-09-24 12:21:46 +0200

    Updated deps and packages

--- mix.exs
+++ mix.exs
@@ -20,6 +20,7 @@ defmodule PhoenixTrello.Mixfile do
     [mod: {PhoenixTrello, []},
      applications: [
       :phoenix,
+      :phoenix_pubsub,
       :phoenix_html,
       :cowboy,
       :logger,
@@ -39,18 +40,19 @@ defmodule PhoenixTrello.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.1.2"},
-      {:phoenix_ecto, "~> 2.0"},
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0.1"},
       {:postgrex, ">= 0.0.0", override: true},
-      {:phoenix_html, "~> 2.3"},
+      {:phoenix_html, "~> 2.6.2"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:cowboy, "~> 1.0"},
-      {:comeonin, "~> 2.0"},
-      {:guardian, "~> 0.9.0"},
-      {:credo, "~> 0.2", only: [:dev, :test]},
-      {:ex_machina, "~> 0.6.1"},
+      {:comeonin, "~> 2.5.3"},
+      {:guardian, "~> 0.13.0"},
+      {:credo, "~> 0.4.11", only: [:dev, :test]},
+      {:ex_machina, "~> 1.0.2"},
       {:exactor, "~> 2.2.0"},
-      {:hound, "~> 0.8"},
+      {:hound, "~> 1.0.2"},
       {:mix_test_watch, "~> 0.2", only: :dev}
      ]
   end
@@ -63,6 +65,7 @@ defmodule PhoenixTrello.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"]]
+     "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
   end
 end
