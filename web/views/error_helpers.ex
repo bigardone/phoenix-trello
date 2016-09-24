5f305c2e7f5e004c3805047da8e517a5174a976b
Ricardo García Vega <bigardone@gmail.com>
2016-09-24 12:26:08 +0200

    Added missing helper

---
+++ web/views/error_helpers.ex
@@ -0,0 +1,22 @@
+defmodule PhoenixTrello.ErrorHelpers do
+  def translate_error({msg, opts}) do
+    # Because error messages were defined within Ecto, we must
+    # call the Gettext module passing our Gettext backend. We
+    # also use the "errors" domain as translations are placed
+    # in the errors.po file.
+    # Ecto will pass the :count keyword if the error message is
+    # meant to be pluralized.
+    # On your own code and templates, depending on whether you
+    # need the message to be pluralized or not, this could be
+    # written simply as:
+    #
+    #     dngettext "errors", "1 file", "%{count} files", count
+    #     dgettext "errors", "is invalid"
+    #
+    if count = opts[:count] do
+      Gettext.dngettext(PhoenixTrello.Gettext, "errors", msg, msg, count, opts)
+    else
+      Gettext.dgettext(PhoenixTrello.Gettext, "errors", msg, opts)
+    end
+  end
+end
