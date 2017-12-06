eb2873ccac24e3017f80778ee97a1de7f1a223b7
ubuntu <tsaizhihan@gmail.com>
2017-12-05 23:46:03 -0700

    fix host

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :phoenix_trello, PhoenixTrello.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "phoenix-trello.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "projectiv.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
