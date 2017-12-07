68252f7f6d9ee403b19a3628b3b0e497dad04d39
ubuntu <tsaizhihan@gmail.com>
2017-12-07 16:56:33 -0700

    list to stage

--- web/static/js/components/lists/form.js
+++ web/static/js/components/lists/form.js
@@ -52,7 +52,7 @@ export default class ListForm extends React.Component {
         <div className="list form">
           <div className="inner">
             <form id="new_list_form" onSubmit={::this._handleSubmit}>
-              <input ref="name" id="list_name" type="text" defaultValue={defaultValue} placeholder="Add a new list..." required="true"/>
+              <input ref="name" id="list_name" type="text" defaultValue={defaultValue} placeholder="Add a new stage..." required="true"/>
               {::this._renderErrors('name')}
               <button type="submit">{buttonText}</button> or <a href="#" onClick={::this._handleCancelClick}>cancel</a>
             </form>
